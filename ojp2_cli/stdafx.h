9c3ffc8ec6366d514848cc85329ea14fe14220a6
Osa__PL <osa12pl@gmail.com>
2016-05-15 13:08:29 +0200

    init commit

---
+++ ojp2_cli/stdafx.h
@@ -0,0 +1,11 @@
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
+#pragma once
+
+// TODO: reference additional headers your program requires here
+#include<windows.h>
+#include<cmath>
+#include<time.h>  
+#include<stdlib.h>
+#include<random>
